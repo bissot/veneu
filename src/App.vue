bc24a2a49360225d741471fac128363cba63bd57
TheStopsign <ejwhitton43@gmail.com>
2021-02-02 04:20:49 -0500

    good ideas flowing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <q-layout id="app" view="lHr Lpr lfr">
-    <q-header elevated class="bg-primary text-white">
+    <q-header class="bg-primary text-white rounded-borders q-ma-md">
       <q-toolbar>
         <q-icon size="sm" flat name="menu" @click="left = !left" />
-
+        <q-input dark borderless v-model="searchString" class="q-ml-md">
+          <template v-slot:append>
+            <q-icon v-if="searchString === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="searchString = ''" />
+          </template>
+        </q-input>
         <q-toolbar-title>
           <q-avatar>
             <img src="./assets/venue-logo.svg" />
@@ -12,46 +17,88 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
-      <q-list bordered class="rounded-borders">
-        <q-expansion-item expand-separator icon="perm_identity" label="Account settings" caption="John Doe">
-          <q-card>
-            <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti commodi
-              magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste eveniet doloribus
-              ullam aliquid.
-            </q-card-section>
-          </q-card>
+    <q-drawer show-if-above v-model="left" side="left">
+      <q-list class="bg-primary text-white rounded-borders q-ml-md q-mt-md q-mb-md">
+        <q-expansion-item
+          expand-separator
+          icon="school"
+          label="Data Structures"
+          caption="CSCI 1200"
+          expand-icon-class="text-white"
+        >
+          <q-list class="rounded-borders">
+            <q-expansion-item
+              expand-separator
+              icon="event_seat"
+              label="Registration Section 02"
+              :header-inset-level="0.25"
+              :content-inset-level="0.25"
+              expand-icon-class="text-white"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              expand-separator
+              icon="groups"
+              label="Team 5"
+              :header-inset-level="0.25"
+              :content-inset-level="0.25"
+              expand-icon-class="text-white"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-list>
         </q-expansion-item>
 
-        <q-expansion-item expand-separator icon="signal_wifi_off" label="Wifi settings">
-          <q-card>
-            <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti commodi
-              magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste eveniet doloribus
-              ullam aliquid.
-            </q-card-section>
-          </q-card>
-        </q-expansion-item>
+        <q-expansion-item expand-separator icon="assignment" label="Assignments" expand-icon-class="text-white">
+          <q-list class="rounded-borders">
+            <q-expansion-item
+              expand-separator
+              icon="assignment_late"
+              label="Due"
+              :header-inset-level="0.25"
+              :content-inset-level="0.25"
+              expand-icon-class="text-white"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
 
-        <q-expansion-item expand-separator icon="drafts" label="Drafts" header-class="text-purple">
-          <q-card>
-            <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti commodi
-              magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste eveniet doloribus
-              ullam aliquid.
-            </q-card-section>
-          </q-card>
-        </q-expansion-item>
-
-        <q-expansion-item icon="assessment" label="Disabled" disable>
-          <q-card>
-            <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti commodi
-              magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste eveniet doloribus
-              ullam aliquid.
-            </q-card-section>
-          </q-card>
+            <q-expansion-item
+              expand-separator
+              icon="assignment_turned_in"
+              label="Complete"
+              :header-inset-level="0.25"
+              :content-inset-level="0.25"
+              expand-icon-class="text-white"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-list>
         </q-expansion-item>
       </q-list>
     </q-drawer>
@@ -80,36 +127,17 @@ export default {
   },
   data() {
     return {
-      left: false
+      left: false,
+      searchString: ""
     };
   }
 };
 </script>
 
 <style>
-/* body {
-  margin: 0;
-  padding: 0;
-} */
 * {
-  font-family: "Comfortaa", Arial, sans-serif;
+  font-family: "Comfortaa", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-/* #app {
-  text-align: center;
-  position: absolute;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-  width: 100vw;
-  height: 100vh;
-  background: var(--venue-light);
-}
-#main {
-  position: absolute;
-  top: 6rem;
-  height: calc(100% - 6rem);
-  width: 100%;
-} */
 </style>
